e1629b0fe19217dd6756e33c146ab9a1f004cbe7
Ryckman, Nathaniel <ryckman1@illinois.edu>
2018-10-05 14:40:48 -0500

    Added RMB sumbodule, config files, and some java/raml code.

---
+++ src/main/java/org/folio/rest/impl/WayfinderAPI.java
@@ -0,0 +1,32 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.core.Response;
+import io.vertx.core.Future;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.folio.rest.jaxrs.model.Wayfinder;
+import org.folio.rest.jaxrs.model.WayfinderCollection;
+import org.folio.rest.jaxrs.resource.WayfindersResource;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.tools.utils.OutStream;
+import org.folio.rest.utils.PgQuery;
+
+public class WayfinderAPI implements WayfindersResource {
+
+	private static final Logger logger = LoggerFactory.getLogger(WayfinderAPI.class);
+
+	private final PostgresClient pgClient;
+	private static final String[] ALL_FIELDS = { "*" };
+	private static final String QUEUERS_TABLE = "wayfinders";
+
+	public WayfinderAPI(Vertx vertx, String tenantId) {
+		this.pgClient = PostgresClient.getInstance(vertx, tenantId);
+		this.pgClient.setIdField("id");
+	}
+}
